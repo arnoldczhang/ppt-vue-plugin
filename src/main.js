2d6393ba3fdcb7f9ee9ab118ff49aeac93692a59
arnold <zhangcheng17@meituan.com>
2017-12-14 09:40:44 +0800

    1.old man`s glasses;

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
 import Vue from 'vue';
 import App from './App';
 import router from './router';
-// import Search from './plugins/Search';
+// import myPlugin from './plugins/myPlugin';
 
 Vue.config.productionTip = false;
-// Vue.use(Search);
+// Vue.use(myPlugin);
 new Vue({
   el: '#app',
   router,
